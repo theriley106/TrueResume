b498dbcbf31c5655063b7f65dec2ed016fd26392
Theriley106 <christopherlambert106@gmail.com>
2019-02-02 16:12:50 -0500

    Added file upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, redirect, jsonify
+import os
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "uploads"
+
+
+@app.route("/")
+def index():
+    return redirect("/static/index.html")
+
+
+@app.route("/sendfile", methods=["POST"])
+def send_file():
+    fileob = request.files["file2upload"]
+    filename = secure_filename(fileob.filename)
+    save_path = "{}/{}".format(app.config["UPLOAD_FOLDER"], filename)
+    fileob.save(save_path)
+
+    # open and close to update the access time.
+    with open(save_path, "r") as f:
+        pass
+
+    return "successful_upload"
+
+
+@app.route("/filenames", methods=["GET"])
+def get_filenames():
+    filenames = os.listdir("resumes/")
+
+    #modify_time_sort = lambda f: os.stat("uploads/{}".format(f)).st_atime
+
+    def modify_time_sort(file_name):
+        file_path = "uploads/{}".format(file_name)
+        file_stats = os.stat(file_path)
+        last_access_time = file_stats.st_atime
+        return last_access_time
+
+    filenames = sorted(filenames, key=modify_time_sort)
+    return_dict = dict(filenames=filenames)
+    return jsonify(return_dict)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
